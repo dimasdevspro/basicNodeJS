a1fcc4571b6805d07e0f004d87c7c44390648c7b
dimasdevspro <dimasdevspro@gmail.com>
2022-01-29 12:52:32 -0300

    basic for NodeJS

---
+++ process.js
@@ -0,0 +1,5 @@
+const firstName = process.argv[2]
+const middleName = process.argv[3]
+const lastName = process.argv[4]
+
+console.log(`Seu nome é ${firstName} ${middleName} ${lastName}`)
---
+++ require.js
@@ -0,0 +1,7 @@
+// const path = require('path')
+// console.log(path.basename(__filename))
+// console.log(require('path'))
+
+const myModule = require('./exports')
+
+console.log(myModule)
